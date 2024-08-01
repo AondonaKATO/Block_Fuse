e40ecda568ff6c6a5dc687f85c3e27edfccb7965
Aondona Kato <152279030+AondonaKATO@users.noreply.github.com>
2024-08-01 11:29:05 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const taskForm = document.getElementById("task-form")
+const taskList = document.getElementById("task-list")
+
+
+
+let taskIndex = 1;
+
+//console.log(taskForm, taskList);
+
+taskForm.addEventListener("submit", function (event){
+    event.preventDefault();
+
+    const taskInput = document.getElementById("task-input");
+    const taskText = taskInput.value.trim();
+
+    //console.log(taskText);
+
+    if(taskText !==""){
+        //Create a new task
+
+        const taskItem = document.createElement("li");
+        taskItem.classList.add("task-item");
+        taskItem.textContent = `${taskIndex}- ${taskText}`;
+
+        taskItem.addEventListener("click", function(){
+            console.log("completed");
+            this.classList.toggle("completed");
+        });
+
+        taskList.appendChild(taskItem);
+
+
+        taskIndex++;
+
+        taskInput.value = "";
+    }
+});
